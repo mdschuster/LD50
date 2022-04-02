e83a38ad1574a63c6146b6d9485bc3dc26e64260
Micah Schuster <micahdschuster@gmail.com>
2022-04-02 10:10:44 -0400

    Add world graphic and health color

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -22,10 +22,21 @@ public class WorldManager : MonoBehaviour
     [Header("World Properties")]
     public float radius;
     public float outerRadius;
+    [Header("World Graphics")]
+    public SpriteRenderer World;
     [Range(0f,1f)]
     public float mood; 
     public Gradient moodColor;
 
+
+    private void Update() {
+        updateWorldColor();
+    }
+
+    public void updateWorldColor() {
+        Color currentColor = moodColor.Evaluate(mood);
+        World.color = currentColor;
+    }
 
 
 
