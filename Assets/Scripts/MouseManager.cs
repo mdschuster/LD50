e05cfcb5eb788a342957a04a295c1afd5480bede
Micah Schuster <micahdschuster@gmail.com>
2022-04-02 20:13:32 -0400

    Add cooldown bar to UI

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseManager : MonoBehaviour
 {
@@ -11,42 +12,88 @@ public class MouseManager : MonoBehaviour
     public GameObject rest;
     public GameObject lightning;
     public float lightningCooldown;
+    public Slider lightningCooldownSlider;
     private float lTime;
+    private float lNormalizedTime;
+    private bool lOnCooldown;
+
+    public GameObject meteor;
+    public float meteorCooldown;
+    public Slider meteorCooldownSlider;
+    private float mTime;
+    private float mNormalizedTime;
+    private bool mOnCooldown;
+
+    private float tol;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        tol = 0.02f;
         lTime = lightningCooldown;
-        //lightning.SetActive(false);
+        lNormalizedTime = lTime / lightningCooldown;
+        lOnCooldown = true;
+
+        mTime = meteorCooldown;
+        mNormalizedTime = mTime / meteorCooldown;
+        mOnCooldown = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        checkLightning();
+        checkMeteor();
+
+
+    }
+
+    private void checkLightning() {
+        if (lOnCooldown) {
+            lNormalizedTime = lTime / lightningCooldown;
+            lOnCooldown=doCooldown(lightningCooldownSlider, lNormalizedTime);
+        }
         lTime -= Time.deltaTime;
-        if (Input.GetMouseButtonDown(0) && lTime<=0f) {
-            
+        if (Input.GetMouseButtonDown(0) && lTime <= 0f) {
+
             Vector2 mousePos = GameManager.Instance().mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 vec = Utility.getNormVectorFromCenter(mousePos)*WorldManager.Instance().radius;
+            Vector2 vec = Utility.getNormVectorFromCenter(mousePos) * WorldManager.Instance().radius;
             GameObject go = Instantiate(lightning, vec, Utility.getQuaternionAlignment(vec));
 
             lTime = lightningCooldown;
-            //TODO need to add graphics to show cooldown
-
-            //lightning.transform.position = vec;
-            //lightning.transform.rotation = Utility.getQuaternionAlignment(vec);
+            lightningCooldownSlider.gameObject.SetActive(true);
+            lOnCooldown = true;
         }
+    }
 
-        //Vector2 mousePos = GameManager.Instance().mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        //Vector2 vec = Utility.getNormVectorFromCenter(mousePos);
-        //innerGizmo.transform.position = vec*WorldManager.Instance().radius;
-        //innerGizmo.transform.rotation = Utility.getQuaternionAlignment(vec);
-        ////print(Utility.getAngleFromVector(new Vector2(mousePos.x,mousePos.y)));
+    private void checkMeteor() {
+        if (mOnCooldown) {
+            mNormalizedTime = mTime / meteorCooldown;
+            mOnCooldown=doCooldown(meteorCooldownSlider, mNormalizedTime);
+        }
+        mTime -= Time.deltaTime;
+        if (Input.GetMouseButtonDown(1) && mTime <= 0f) {
 
+            Vector2 mousePos = GameManager.Instance().mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 vec = Utility.getNormVectorFromCenter(mousePos) * WorldManager.Instance().radius;
+            GameObject go = Instantiate(meteor, vec, Utility.getQuaternionAlignment(vec));
 
-        //outerGizmo.transform.position = vec * WorldManager.Instance().outerRadius;
+            mTime = meteorCooldown;
+            meteorCooldownSlider.gameObject.SetActive(true);
+            mOnCooldown = true;
+        }
+    }
 
-
+    private bool doCooldown(Slider slider, float time) {
+        slider.value = time;
+        bool onCooldown = true;
+        if (Mathf.Abs(slider.value - tol) < 0.02f) {
+            onCooldown = false;
+            slider.gameObject.SetActive(false);
+        }
+        return onCooldown;
     }
 }
